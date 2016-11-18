46a02d2a42ca9a8e90ec0645ca875899fbb1312d
Magnus Hagmar <hagmar.magnus@gmail.com>
2016-11-19 00:14:54 +0100

    Moved all message handling to snake class

--- main.py
+++ main.py
@@ -1,30 +1,13 @@
 #!/usr/bin/python3
-import snakesock
-import messages
-import json
 import snake
-
-def route_message(message):
-    json_msg = json.loads(message)
-    msg_type = json_msg['type']
-    msg_handler = snake.get_message_handler(msg_type)
-    msg_handler()
 
 def main():
     my_snake = snake.Snake()
+    my_snake.connect()
+    my_snake.register()
 
-    sock = snakesock.Snakesock()
-    sock.connect("snake.cygni.se", 80)
-
-    sock.send(json.dumps(messages.client_info()))
-    sock.send(json.dumps(messages.player_registration(my_snake.name)))
-
-    while True:
-        message = sock.recv()
-        if not message:
-            break
-        route_message(message)
-
+    while my_snake.connected():
+        my_snake.receive()
 
 if __name__ == '__main__':
     main()
