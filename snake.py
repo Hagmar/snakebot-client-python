01442e0a4d9395b72a6098570cedaf8ec59fa782
Magnus Hagmar <hagmar.magnus@gmail.com>
2016-11-19 17:59:44 +0100

    Removed explicit call to basesnake constructor

--- snake.py
+++ snake.py
@@ -2,9 +2,6 @@ from basesnake import BaseSnake
 from config import LOGGING_LEVEL
 
 class Snake(BaseSnake):
-    def __init__(self, logging_level=LOGGING_LEVEL):
-        super().__init__(logging_level)
-
     def get_next_move(self, game_map):
         move = "DOWN"
         return move
